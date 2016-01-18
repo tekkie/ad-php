72387439c8aebdab36d9a57e92ce01f70cb722e7
Georgiana Gligor <gb@tekkie.ro>
2016-01-18 13:20:56 +0200

    defined dependencies + added test

---
+++ README.md
@@ -0,0 +1,6 @@
+# Active Directory test for PHP
+
+## Installation
+
+Make sure you have Composer installed.
+TODO
---
+++ demo.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Adldap\Adldap;
+use Adldap\Exceptions\AdldapException;
+
+$configuration = array(
+  'account_suffix' => '@gatech.edu',
+  'domain_controllers' => array('whitepages.gatech.edu'),
+  'port' => 389,
+  'base_dn' => 'dc=whitepages,dc=gatech,dc=edu',
+  'admin_username' => '',
+  'admin_password' => '',
+  'follow_referrals' => true,
+  'use_ssl' => false,
+  'use_tls' => false,
+  'use_sso' => false,
+);
+
+try {
+  $connection = new Adldap($configuration);
+
+  echo 'Connection details:' . "\n";
+  var_dump($connection);
+} catch (AdldapException $ex) {
+  echo sprintf('Connection issue:%s%s', "\n\t", $ex->getMessage());
+}
